2af1ab0b85ce1064feaba03039a0d88ec8059977
Daniel Senften <daniel@senften.org>
2020-06-17 17:16:23 +0200

    Some more examples added during day 1

---
+++ exercise/day_1/menu.py
@@ -0,0 +1,19 @@
+menu = {
+    'en': {
+        'file': 'File',
+        'new': 'New',
+        'open': 'Open',
+        'save': 'Save',
+        'save_as': 'Save as'
+    },
+    'de': {
+        'file': 'Datei',
+        'new': 'Neu',
+        'open': 'Öffnen',
+        'save': 'Speichern',
+        'save_as': 'Speichern als'
+    },
+
+}
+
+language = input('Sprache: ')
