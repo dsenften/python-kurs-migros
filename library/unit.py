daf614363cd4b1e1480a5b609917c03f2abb4b01
Daniel Senften <daniel@senften.org>
2020-09-30 15:48:24 +0200

    Integrate some test cases

--- library/unit.py
+++ library/unit.py
@@ -95,3 +95,26 @@ class Length:
 
     def __repr__(self):
         return str((self.value, self.unit))
+
+
+if __name__ == "__main__":
+    """
+    Die folgenden Aufrufe sollten eigentlich aus einer externen Umgebung
+    aufgerufen werden, da wir mit dieser Datei/Klasse auch die Verwendung 
+    von eigenen Modulen/Libraries aufzeigen wollen. 
+    """
+
+    x = Length(4)
+    print(x)
+    print(repr(x))
+    print(x + 5.6)
+    print(7 + x)
+    x += Length(4, "yd")
+    print(x)
+    x += 4
+    x -= Length(4, "m")
+    print(x)
+    z = 4 + Length(8, "yd")
+    y = Length(4.5, "yd") + x / Length(0.77, "in")
+    print(repr(y))
+    print(y)
