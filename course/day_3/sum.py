a65421e31450883d1507a24b35164eadc04364a2
Daniel Senften <daniel@senften.org>
2020-09-30 16:07:32 +0200

    Some more examples

--- course/day_3/sum.py
+++ course/day_3/sum.py
@@ -3,3 +3,7 @@ n = 101
 print(sum(range(1, 101)))
 
 print(list(range(2, 10, 2)))
+
+print(1, 2, sep=' --- ')
+print('Daniel', end=' ')
+print('Senften')
---
+++ fibonacci_test.py
@@ -0,0 +1,4 @@
+from fibonacci import functions
+
+print(functions.fib(10))
+print(functions.fiblist(10))
