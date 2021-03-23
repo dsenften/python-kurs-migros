151fef80a9029089562bc4ba2b0922957adaa795
Daniel Senften <daniel.senften@talent-factory.ch>
2021-03-23 22:31:25 +0100

    Singleton added

---
+++ patterns/singleton/__init__.py
@@ -0,0 +1,6 @@
+"""
+Als Singleton bezeichnet man ein Objekt, das innerhalb eines laufenden Programms nur in einer Ausprägung („Instanz“)
+existieren darf; beispielsweise ist bei jedem Betriebsystem mit grafischer Oberfläche genau ein Window-Manager in
+Betrieb. Zugleich muss das Singleton-Objekt unter Umständen für viele Stellen zugriffsbereit sein.
+"""
+
